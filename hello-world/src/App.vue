1f433dfee51d18a8c1d7b5d1292d9d09b3b24170
Arne Glenstrup <arne.glenstrup@mancofi.dk>
2018-11-08 08:31:16 +0100

    Added axios plugin, removed unused cs file

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Hello World</v-toolbar-title>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            <v-list v-for="company in companies" :key="company.id">
+              <v-list-tile>
+                {{ company.name }}
+              </v-list-tile>
+            </v-list>
+            <v-list v-for="error in errors" :key="error">
+              <v-list-tile>Error: {{ error }}</v-list-tile>
+            </v-list>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer color="indigo" app>
+      &nbsp; <span class="white--text">&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import axios from 'axios'
 import HelloWorld from './components/HelloWorld'
 
 export default {
@@ -29,10 +62,19 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
-    return {
-      //
-    }
+  data: () => ({
+    drawer: null,
+    errors: [],
+    companies: [
+      { "id" : 4, "name" : "SkunkPension" },
+      { "id" : 5, "name" : "EfterLiv Forsikring" },
+      { "id" : 6, "name" : "SælvForsikring" } ]
+  }),
+  created() {
+    axios.get("https://arnes-hello-world.azurewebsites.net/api/HelloWorld")
+    .then(response => new Promise(resolve => setTimeout(() => resolve(response), 2000)))
+    .then(response => { this.companies = response.data; })
+    .catch(e => this.errors.push(e));
   }
 }
 </script>
